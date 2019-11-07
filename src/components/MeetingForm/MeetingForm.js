525e2bd367f2f175dd82be9cfe4bd2c563688845
Nat Sharpe <sharpe1890@gmail.com>
2019-11-06 23:56:54 -0500

    Input form in meeting modal

--- src/components/MeetingForm/MeetingForm.js
+++ src/components/MeetingForm/MeetingForm.js
@@ -4,24 +4,42 @@ import './MeetingForm.css';
 
 class MeetingForm extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      phone: '',
+    };
+  }
+
+  handleChange = (e) => {
+    console.log(this.state);
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
   render() {
     const { meetings, selectedId } = this.props;
     const { time, name, phone } = meetings[selectedId] ? meetings[selectedId] : "";
 
-    return ( 
-      <div>
-        <div>
-          {time}
-        </div>
-        <div>
-          {name}
-        </div>
-        <div>
-          {phone}
-        </div>
-      </div>
-    );
+    return (
+      <React.Fragment>
+        <h3>{ time }</h3>
+        <form>
+          <p>Name: </p>
+          <div>
+            <input type="text" name="name" value={ name } onChange={ this.handleChange } />
+          </div>
+          <p>Phone: </p>
+          <div>
+            <input name="phone" value={ phone } onChange={ this.handleChange }></input>
+          </div>
+          <button type="submit">Save</button>
+        </form>
+      </React.Fragment>
+    )
   }
+
 };
 
 const mapStateToProps = state => {
